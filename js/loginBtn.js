a4c3d3f8af0adc19740112c1b3cda6a76675c7e1
modernaccademy <mmaa.1252000@gmail.com>
2021-07-12 23:31:19 +0200

    ch2

---
+++ js/loginBtn.js
@@ -0,0 +1,20 @@
+function myFunction() {
+    var Username = document.getElementById("usernameField").value;
+    var Password = document.getElementById("passwordField").value;
+    if (Username == '4190283' && Password == '104158') {
+        window.open("result.html");
+        document.getElementById("bodyindex").style.backgroundImage = "url(images/KarimWaelGrades.png)";
+    } else if (Username == '4190283' && Password != '104158') {
+        alert('Check Your Username Or Password');
+    } else if (Username == '4200202' && Password == '101351') {
+        window.open("result2.html");
+    } else if (Username == '4200202' && Password != '101351') {
+        alert('Check Your Username Or Password');
+    } else if (Username == '4190261' && Password == '103215') {
+        window.open("result2.html");
+    } else if (Username == '4190261' && Password != '103215') {
+        alert('Check Your Username Or Password');
+    } else {
+        alert('Invalid Login!!!');
+    }
+};
