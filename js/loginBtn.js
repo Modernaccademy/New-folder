92cebbb170ad3bf0b4c95c62bbef84cd113eb0d4
Modernaccademy <82056032+Modernaccademy@users.noreply.github.com>
2024-02-04 13:53:06 +0000

    new commint 2024

--- js/loginBtn.js
+++ js/loginBtn.js
@@ -2,9 +2,9 @@ function myFunction() {
     var Username = document.getElementById("usernameField").value;
     var Password = document.getElementById("passwordField").value;
     if (Username == '4190283' && Password == '104158') {
-        window.open("result.html");
-    } else if (Username == '4200202' && Password == '101351') {
         window.open("result2.html");
+    } else if (Username == '4200202' && Password == '101351') {
+        window.open("result.html");
     }  else if (Username == '4190261' && Password == '103215') {
         window.open("result3.html");
     }  else if (Username == '4200296' && Password == '105331') {
